e46933ce1a21fc00c67fd0fabb0fa4d2c19986a8
jcarroyos <jcarroyos@gmail.com>
2024-10-19 08:54:10 -0500

    minor fixies

--- backend/serial/status.ts
+++ backend/serial/status.ts
@@ -2,32 +2,24 @@ import { api } from "encore.dev/api";
 import { SerialPort } from "serialport";
 import { ReadlineParser } from "@serialport/parser-readline";
 
-// Replace with your serial port path
-const port = new SerialPort({ path: "/dev/tty.usbmodem14101", baudRate: 9600 });
+const port = new SerialPort({ path: "/dev/tty.usbmodem14601", baudRate: 9600 });
 const parser = port.pipe(new ReadlineParser());
 
-// Variable to store the last complete line of data
 let lastLine = "";
 
-// Function to handle incoming data
 const handleData = (data: string) => {
   const lines = data.split("\r");
-
-  // Store the last complete line
   if (lines.length > 1) {
     lastLine = lines[lines.length - 2];
   }
 };
 
-// Function to handle errors
 port.on("error", (err) => {
   console.error("Error: ", err.message);
 });
 
-// Set up event listeners for the serial port
 parser.on("data", handleData);
 
-// Define the Encore endpoint to get serial data
 interface Response {
   data: string | { error: string };
 }
@@ -46,7 +38,6 @@ export const getSerialData = api(
   }
 );
 
-// Define the new Encore endpoint to send data to the Arduino Esplora
 export const sendDataToEsplora = api(
   {
     expose: true,
--- stop.sh
+++ stop.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Define the ports
 ports=(9400 4000 5173)
 
-# Loop through each port and kill the process running on it
 for port in "${ports[@]}"; do
   echo "Checking for processes on port $port..."
-  pids=$(netstat -ano | grep ":$port" | awk '{print $5}' | sort | uniq)
+  pids=$(lsof -t -i :$port)
   if [ -n "$pids" ]; then
     for pid in $pids; do
       if [ "$pid" -ne 0 ]; then
         echo "Stopping process on port $port (PID: $pid)"
-        taskkill //PID $pid //F
+        kill -9 $pid
         if [ $? -eq 0 ]; then
           echo "Successfully stopped process on port $port (PID: $pid)"
         else
@@ -23,8 +21,3 @@ for port in "${ports[@]}"; do
     echo "No process found on port $port"
   fi
 done
-
-echo "All specified processes have been stopped."
-
-# Keep the bash window open
-read -p "Press any key to exit..."
